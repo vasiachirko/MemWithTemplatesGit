ec5bb3a523b33bce9ef9b2f66dcf66b3cbade3d4
Чирко Василий Олегович <51925240+vasiachirko@users.noreply.github.com>
2023-09-24 17:36:36 +0300

    Scale

--- app/src/main/java/com/example/memwithtemplates/MainActivity.kt
+++ app/src/main/java/com/example/memwithtemplates/MainActivity.kt
@@ -5,19 +5,38 @@ import android.app.ActionBar.LayoutParams
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.GridView
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TableRow
 import androidx.activity.ComponentActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginStart
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : ComponentActivity(), View.OnClickListener {
+
+    val currentLevel = 3
+    var initialClick = false
+    val randomListIndexes = ArrayList<Int>()
+    val allButtonsOnPage = ArrayList<ImageButton>()
+    val picturesList = ArrayList<Int>()
+
+    lateinit var imgRight : ImageView
+    lateinit var imgLeft : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initialClick = true
         setContentView(R.layout.activity_main)
         createLayout()
-        val picturesList = ArrayList<Int>()
 
+        val mainScreenMainLayout: ConstraintLayout = findViewById(R.id.mainConstraintLayout)
+        mainScreenMainLayout.setOnClickListener(this)
         picturesList.add(R.drawable.pic0)
         picturesList.add(R.drawable.pic1)
         picturesList.add(R.drawable.pic2)
@@ -34,8 +53,11 @@ class MainActivity : ComponentActivity() {
         picturesList.add(R.drawable.pic13)
         picturesList.add(R.drawable.pic14)
 
-        val randomListIndexes = ArrayList<Int>()
-        for (i in 0..< 10){
+
+
+
+
+        for (i in 0..< currentLevel * 10 / 2){
             randomListIndexes.add(i)
             randomListIndexes.add(i)
         }
@@ -43,15 +65,10 @@ class MainActivity : ComponentActivity() {
 
 
 
-        for (i in 0..<20) {
-            val my_btn : ImageButton = findViewById(("+id/btn" + i.toString()).hashCode())
+        for (i in 0..<currentLevel * 10) {
+            val my_btn : ImageButton = allButtonsOnPage[i]
             my_btn.setImageResource(picturesList[randomListIndexes[i]])
-        }
-
-
-        for (i in 0..<10) {
-            val my_btn : ImageButton = findViewById(("+id/btn" + (i+10).toString()).hashCode())
-            my_btn.setImageResource(picturesList[i])
+            my_btn.setOnClickListener(this)
         }
     }
 
@@ -63,86 +80,126 @@ class MainActivity : ComponentActivity() {
         //detach 2 images and buttons
         val picturesPlusButtonsLayoutMain = LinearLayout(this)
         picturesPlusButtonsLayoutMain.orientation = LinearLayout.VERTICAL
-        picturesPlusButtonsLayoutMain.gravity = Gravity.CENTER
-        val lp3 = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        picturesPlusButtonsLayoutMain.layoutParams = lp3
+        picturesPlusButtonsLayoutMain.gravity = Gravity.TOP
+        val lp = ConstraintLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        picturesPlusButtonsLayoutMain.layoutParams = lp
         mainScreenMainLayout.addView(picturesPlusButtonsLayoutMain)
 
-        //images
-        val twoMainPicturesLayout = LinearLayout(this)
-        twoMainPicturesLayout.orientation = LinearLayout.HORIZONTAL
-        twoMainPicturesLayout.gravity = Gravity.CENTER
-        val lp = LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        lp.setMargins(0, 0, 0, 100)                        //between pictures and buttons
-        twoMainPicturesLayout.layoutParams = lp
-        picturesPlusButtonsLayoutMain.addView(twoMainPicturesLayout)
+
+        val picturesLayout = TableLayout(this)
+        val lpTable = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        lpTable.gravity = Gravity.TOP
+        picturesLayout.layoutParams = lpTable
+        picturesPlusButtonsLayoutMain.addView(picturesLayout)
+
+        val tableRow = TableRow(this)
+        val lpRow = TableLayout.LayoutParams()
+        lpRow.width = TableLayout.LayoutParams.WRAP_CONTENT
+        lpRow.height = TableLayout.LayoutParams.WRAP_CONTENT
+        lpRow.gravity = Gravity.CLIP_HORIZONTAL
+        picturesLayout.addView(tableRow)
+
+
+
 
         //2 pictures
-        val imgLeft = ImageView(this)
-        imgLeft.maxWidth = 300
-        imgLeft.maxHeight = 300
+        imgLeft = ImageView(this)
         imgLeft.adjustViewBounds = true
+        val lpImgLeft = TableRow.LayoutParams()
+        lpImgLeft.width = TableRow.LayoutParams.WRAP_CONTENT
+        lpImgLeft.height = TableRow.LayoutParams.WRAP_CONTENT
+        lpImgLeft.weight = 1.0f
+        imgLeft.layoutParams = lpImgLeft
         imgLeft.setImageResource(R.drawable.tshirt2)
-        twoMainPicturesLayout.addView(imgLeft)
+        tableRow.addView(imgLeft)
 
-        val lp2 = LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        lp2.setMargins(0, 0, 50, 0)
-        imgLeft.layoutParams = lp2
 
-        val imgRight = ImageView(this)
-        imgRight.maxWidth = 300
-        imgRight.maxHeight = 300
+
+        imgRight = ImageView(this)
         imgRight.adjustViewBounds = true
+        val lpImgRight = TableRow.LayoutParams()
+        lpImgRight.height = TableRow.LayoutParams.WRAP_CONTENT
+        lpImgRight.width = TableRow.LayoutParams.WRAP_CONTENT
+        lpImgRight.weight = 1.0f
+        imgRight.layoutParams = lpImgRight
         imgRight.setImageResource(R.drawable.tshirt2)
-        twoMainPicturesLayout.addView(imgRight)
+
+        tableRow.addView(imgRight)
 
         //layout and buttons itself
-        val buttonsLayouts = ArrayList<LinearLayout>()
-        val layoutParamsList = ArrayList<LinearLayout.LayoutParams>()
-        val allButtonsOnPage = ArrayList<ImageButton>()
+        val buttonsLayout = TableLayout(this)
+        val lpButtonsTable = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        picturesPlusButtonsLayoutMain.addView(buttonsLayout)
 
-        val numOfRows = 4
+        val numOfButtonsInARow = 5
+        val numOfRows = currentLevel * 10 / numOfButtonsInARow
         for (j in 0..<numOfRows) {
-            buttonsLayouts.add(LinearLayout(this))
-            buttonsLayouts[j].orientation = LinearLayout.HORIZONTAL
-            buttonsLayouts[j].gravity = Gravity.CENTER
-            layoutParamsList.add(
-                LinearLayout.LayoutParams(
-                    LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT
-                )
-            )
-            layoutParamsList[j].setMargins(0, 0, 0, 25)        //margin to next row
-            buttonsLayouts[j].layoutParams = layoutParamsList[j]
-            picturesPlusButtonsLayoutMain.addView(buttonsLayouts[j])
+            val tableRowButtons = TableRow(this)
+            val lpRowButtons = TableLayout.LayoutParams()
+            lpRowButtons.width = TableLayout.LayoutParams.WRAP_CONTENT
+            lpRowButtons.height = TableLayout.LayoutParams.WRAP_CONTENT
+            lpRowButtons.gravity = Gravity.CLIP_HORIZONTAL
+            tableRowButtons.layoutParams = lpRowButtons
+            buttonsLayout.addView(tableRowButtons)
 
-            val numOfButtonsInARow = 5
+
+
             for (i in 0..<numOfButtonsInARow) {
                 allButtonsOnPage.add(ImageButton(this))
                 val index = numOfButtonsInARow * j + i
-                allButtonsOnPage[index].id = ("+id/btn" + index.toString()).hashCode()
+                val lpImageButton = TableRow.LayoutParams()
+                lpImageButton.width = TableRow.LayoutParams.WRAP_CONTENT
+                lpImageButton.height = TableRow.LayoutParams.WRAP_CONTENT
+                lpImageButton.weight = 1.0f
+
+                allButtonsOnPage[index].layoutParams = lpImageButton
+                allButtonsOnPage[index].tag = index
                 allButtonsOnPage[index].adjustViewBounds = true
-                allButtonsOnPage[index].maxWidth = 130
-                allButtonsOnPage[index].maxHeight = 130
+//                allButtonsOnPage[index].maxWidth = 130
+//                allButtonsOnPage[index].maxHeight = 130
                 allButtonsOnPage[index].setImageResource(R.drawable.tshirt2)
-                buttonsLayouts[j].addView(allButtonsOnPage[index])
+                tableRowButtons.addView(allButtonsOnPage[index])
 
             }
         }
-//
-//        allButtonsOnPage.add(ImageButton(this))
-//        val lastButton = allButtonsOnPage[allButtonsOnPage.size-1]
-//        lastButton.id = ("+id/btn" + "1").hashCode()
-//        lastButton.maxWidth = 250
-//        lastButton.maxHeight = 250
-//        lastButton.adjustViewBounds = true
-//        lastButton.setImageResource(R.drawable.tshirt2)
-//        buttonsLayouts[buttonsLayouts.size-1].addView(lastButton)
-//        val template = "+id/btn"
-//        val searchId = template + "1"
-//        findViewById<ImageButton>(searchId.hashCode()).setImageResource(R.drawable.ic_launcher_foreground)
-//
-//        allButtonsOnPage[10].setImageResource(R.drawable.ic_launcher_foreground)
-//        allButtonsOnPage[10].visibility = View.INVISIBLE
+
+    }
+
+    var buttonLeft : ImageButton? = null
+    var buttonRight : ImageButton? = null
+    override fun onClick(v: View) {
+        if (initialClick) {
+            for (i in 0..<currentLevel * 10) {
+                val my_btn : ImageButton = allButtonsOnPage[i]
+                my_btn.setImageResource(R.drawable.tshirt2)
+            }
+            initialClick = false
+            return
+        }
+
+
+
+        if (buttonLeft == null && v.tag != null ) {
+            buttonLeft = allButtonsOnPage[v.tag as Int]
+            allButtonsOnPage[v.tag as Int].setImageResource(picturesList[randomListIndexes[v.tag as Int]])
+            imgLeft.setImageResource(picturesList[randomListIndexes[v.tag as Int]])
+        } else if (buttonRight == null && v.tag != null && buttonLeft!!.tag != v.tag) {
+            buttonRight = allButtonsOnPage[v.tag as Int]
+            allButtonsOnPage[v.tag as Int].setImageResource(picturesList[randomListIndexes[v.tag as Int]])
+            imgRight.setImageResource(picturesList[randomListIndexes[v.tag as Int]])
+            if (randomListIndexes[buttonLeft!!.tag as Int] == randomListIndexes[buttonRight!!.tag as Int]) {
+                buttonLeft!!.visibility = View.INVISIBLE
+                buttonRight!!.visibility = View.INVISIBLE
+            }
+        } else if (buttonLeft != null && buttonRight != null ){
+            if (buttonLeft != null){ buttonLeft!!.setImageResource(R.drawable.tshirt2) }
+            if (buttonRight != null){ buttonRight!!.setImageResource(R.drawable.tshirt2) }
+            imgLeft.setImageResource(R.drawable.tshirt2)
+            imgRight.setImageResource(R.drawable.tshirt2)
+            buttonLeft = null
+            buttonRight = null
+        }
+
+
     }
 }
